c1c74d74d3878c306752eee8a6bd7823176c49aa
Stichoza <stichoza@gmail.com>
2014-01-15 06:13:31 +0400

    typeahed try

--- Gruntfile.js
+++ Gruntfile.js
@@ -15,6 +15,12 @@ module.exports = function(grunt) {
 					title: "Comapass:",
 					message: "compilation complete"
 				}
+			},
+			jsmin: {
+				options: {
+					title: "JavaScript:",
+					message: "concat and uglify"
+				}
 			}
 		},
 		compass: {
@@ -28,6 +34,29 @@ module.exports = function(grunt) {
 					javascriptsDir: 'frontend/js',
 					imagesDir: 'frontend/images',
 					httpPath : 'frontend/'
+				}
+			}
+		},
+		concat: {
+			options: {
+
+			},
+			dist: {
+				src: [
+					"frontend/js/youtube.js",
+					"frontend/js/lastfm.js",
+					"frontend/js/main.js"
+				],
+				dest: "frontend/js/dist/ytlfm.con.js"
+			}
+		},
+		uglify: {
+			options: {
+				banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
+			},
+			dist: {
+				files: {
+				'frontend/js/dist/ytlfm.min.js': ['<%= concat.dist.dest %>']
 				}
 			}
 		},
@@ -59,7 +88,11 @@ module.exports = function(grunt) {
 		watch: {
 			sass: {
 				files: ['frontend/sass/*.{sass,scss}','frontend/sass/*/*.{sass,scss}'],
-				tasks: ['compass', 'notify']
+				tasks: ['compass'] //, 'notify:sass'
+			},
+			jsmin: {
+				files: ['frontend/js/*.js'],
+				tasks: ['concat', 'uglify'] //, 'notify:jsmin'
 			},
 			assets: {
 				files: [
@@ -78,6 +111,8 @@ module.exports = function(grunt) {
 	grunt.loadNpmTasks('grunt-contrib-watch');
 	grunt.loadNpmTasks('grunt-contrib-connect');
 	grunt.loadNpmTasks('grunt-contrib-compass');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-concat');
 	grunt.loadNpmTasks('grunt-notify');
 
 	grunt.registerTask('default', [
